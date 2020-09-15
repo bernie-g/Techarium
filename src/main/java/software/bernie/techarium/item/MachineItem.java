d231a1ca09aef490f02b1711d8fc01cc76afeb2f
Drizzs <drizzs@hotmail.com>
2020-09-15 13:43:01 -0400

    Machine Item Name + Tooltip

--- src/main/java/software/bernie/techarium/item/MachineItem.java
+++ src/main/java/software/bernie/techarium/item/MachineItem.java
@@ -26,19 +26,27 @@ public class MachineItem extends BlockItem {
     @Override
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
         int tier = 0;
-        if(stack.hasTag() && stack.getTag() != null){
+        if (stack.hasTag() && stack.getTag() != null) {
             tier = stack.getTag().getInt("tier");
         }
-        tooltip.add(new TranslationTextComponent("block.techarium.botarium_tier_" + tier));
+        tooltip.add(new TranslationTextComponent(getTranslationKey() + "_tier_" + tier));
         super.addInformation(stack, worldIn, tooltip, flagIn);
+    }
+
+    @Override
+    public String getTranslationKey(ItemStack stack) {
+        if(stack.hasTag() && stack.getTag()!= null){
+            return super.getTranslationKey(stack) + "_tier_" +stack.getTag().getInt("tier");
+        }
+        return super.getTranslationKey(stack);
     }
 
     @Override
     public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
         if (this.isInGroup(group)) {
-            for(int x = 1; x < tiers;++x){
+            for (int x = 1; x <= tiers; ++x) {
                 CompoundNBT nbt = new CompoundNBT();
-                nbt.putInt("tier",x);
+                nbt.putInt("tier", x);
                 ItemStack stack = this.getDefaultInstance();
                 stack.setTag(nbt);
                 items.add(stack);
