d957581e1313e4cc5a4d59c3487def87eb242aaf
bernie-g <berniegandin@gmail.com>
2020-09-15 00:05:56 -0400

    Fixed wack rendering

--- src/main/java/software/bernie/techarium/client/tile/render/BotariumRenderer.java
+++ src/main/java/software/bernie/techarium/client/tile/render/BotariumRenderer.java
@@ -6,7 +6,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.Quaternion;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
-import net.minecraft.client.renderer.texture.AtlasTexture;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.util.ResourceLocation;
 import software.bernie.geckolib.geo.render.GeoBlockRenderer;
@@ -46,16 +45,15 @@ public class BotariumRenderer extends GeoBlockRenderer<BotariumTile>
 		return location;
 	}
 
+
 	@Override
-	public void renderCustom(BotariumTile tile, MatrixStack stackIn, float ticks, MatrixStack matrixStackIn, IRenderTypeBuffer renderTypeBuffer, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float partialTicks)
+	public void renderCustom(BotariumTile tile, MatrixStack matrixStackIn, float ticks, IRenderTypeBuffer renderTypeBuffer, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float partialTicks)
 	{
-		stackIn.push();
-		stackIn.translate(0, 0.61, -0.5);
-		stackIn.rotate(new Quaternion(25, 0, 0, true));
-		stackIn.scale(0.21f, 0.21f, 0.21f);
-		Minecraft.getInstance().getTextureManager().bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
+		matrixStackIn.push();
+		matrixStackIn.translate(0, 0.61, -0.5);
+		matrixStackIn.rotate(new Quaternion(25, 0, 0, true));
+		matrixStackIn.scale(0.21f, 0.21f, 0.21f);
 		Minecraft.getInstance().getItemRenderer().renderItem(tile.getCropInventory().getStackInSlot(0), ItemCameraTransforms.TransformType.NONE, packedLightIn, packedOverlayIn, matrixStackIn, renderTypeBuffer);
-		Minecraft.getInstance().getTextureManager().bindTexture(getTexture(tile));
-		stackIn.pop();
+		matrixStackIn.pop();
 	}
 }
