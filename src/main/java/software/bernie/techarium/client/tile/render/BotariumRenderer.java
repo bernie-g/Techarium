b3d0bfba78343a2a3f965c18781ef1c9c5e7a804
bernie-g <berniegandin@gmail.com>
2020-09-14 22:02:11 -0400

    Fixed rendering bug maybe?

--- src/main/java/software/bernie/techarium/client/tile/render/BotariumRenderer.java
+++ src/main/java/software/bernie/techarium/client/tile/render/BotariumRenderer.java
@@ -6,6 +6,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.Quaternion;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.texture.AtlasTexture;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.util.ResourceLocation;
 import software.bernie.geckolib.geo.render.GeoBlockRenderer;
@@ -52,7 +53,9 @@ public class BotariumRenderer extends GeoBlockRenderer<BotariumTile>
 		stackIn.translate(0, 0.61, -0.5);
 		stackIn.rotate(new Quaternion(25, 0, 0, true));
 		stackIn.scale(0.21f, 0.21f, 0.21f);
-		Minecraft.getInstance().getItemRenderer().renderItem(tile.getCropInventory().getStackInSlot(0), ItemCameraTransforms.TransformType.GUI, packedLightIn, packedOverlayIn, matrixStackIn, renderTypeBuffer);
+		Minecraft.getInstance().getTextureManager().bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
+		Minecraft.getInstance().getItemRenderer().renderItem(tile.getCropInventory().getStackInSlot(0), ItemCameraTransforms.TransformType.NONE, packedLightIn, packedOverlayIn, matrixStackIn, renderTypeBuffer);
+		Minecraft.getInstance().getTextureManager().bindTexture(getTexture(tile));
 		stackIn.pop();
 	}
 }
