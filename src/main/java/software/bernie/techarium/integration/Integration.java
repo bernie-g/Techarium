4f230fabd1e5bc2b83638adb0dbaf83bac38fed1
bernie-g <berniegandin@gmail.com>
2021-05-11 10:28:05 -0400

    Made integrations use Lazy's

--- src/main/java/software/bernie/techarium/integration/Integration.java
+++ src/main/java/software/bernie/techarium/integration/Integration.java
@@ -1,42 +1,36 @@
 package software.bernie.techarium.integration;
 
+import lombok.Getter;
+import net.minecraftforge.common.util.Lazy;
 import net.minecraftforge.fml.ModList;
 
+import java.util.function.Supplier;
+
 public abstract class Integration {
-    private final String modID;
-
-    protected Integration(String modID) {
-        this.modID = modID;
-    }
-
-    public boolean isPresent() {
-        return ModList.get().isLoaded(this.getModID());
-    }
-
-    public String getModID() {
-        return this.modID;
-    }
 
     public static class Wrapper<T extends Integration> {
-        private final T integration;
+        @Getter
+        private static String modID;
+        private final Lazy<T> integration;
 
-        public static <T extends Integration> Wrapper<T> of(T integration) {
-            return new Wrapper(integration);
+        public static <T extends Integration> Wrapper<T> of(String modID, Supplier<T> integration) {
+            Wrapper.modID = modID;
+            return new Wrapper(Lazy.of(integration));
         }
 
-        public Wrapper(T integration) {
+        public Wrapper(Lazy<T> integration) {
             this.integration = integration;
         }
 
         public boolean isPresent() {
-            return integration.isPresent();
+            return ModList.get().isLoaded(this.getModID());
         }
 
         public T get() {
             if (isPresent()) {
-                return this.integration;
+                return this.integration.get();
             } else {
-                throw new ModIntegrationException(integration.getModID());
+                throw new ModIntegrationException(this.getModID());
             }
         }
     }
--- src/main/java/software/bernie/techarium/integration/ModIntegrations.java
+++ src/main/java/software/bernie/techarium/integration/ModIntegrations.java
@@ -3,6 +3,5 @@ package software.bernie.techarium.integration;
 import software.bernie.techarium.integration.mekanism.MekanismIntegration;
 
 public class ModIntegrations {
-    public static final Integration.Wrapper<MekanismIntegration> MEKANISM = Integration.Wrapper.of(
-            new MekanismIntegration("mekanism"));
+    public static final Integration.Wrapper<MekanismIntegration> MEKANISM = Integration.Wrapper.of("mekanism", () -> new MekanismIntegration());
 }
