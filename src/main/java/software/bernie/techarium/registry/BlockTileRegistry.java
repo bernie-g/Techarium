5da927d286ecc8ce9d62647e9723eba95f5e15e4
agnor99 <36055315+agnor99@users.noreply.github.com>
2021-05-11 14:57:55 +0200

    Added Position check (#5)
    Looks good!

--- src/main/java/software/bernie/techarium/registry/BlockTileRegistry.java
+++ src/main/java/software/bernie/techarium/registry/BlockTileRegistry.java
@@ -10,8 +10,9 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import software.bernie.techarium.Techarium;
 import software.bernie.techarium.block.BlockRegistryObjectGroup;
+import software.bernie.techarium.block.base.MachineBlock;
 import software.bernie.techarium.block.botarium.BotariumTop;
-import software.bernie.techarium.block.botarium.BotaniumMaster;
+import software.bernie.techarium.block.botarium.BotariumMaster;
 import software.bernie.techarium.item.MachineItem;
 import software.bernie.techarium.tile.botarium.BotariumTile;
 import software.bernie.techarium.tile.slaves.TopEnabledOnlySlave;
@@ -28,19 +29,19 @@ public class BlockTileRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Techarium.ModID);
 
     //Tile
-    public static final BlockRegistryObjectGroup<BotaniumMaster, BlockItem, BotariumTile> BOTARIUM =
-            new BlockRegistryObjectGroup<>("botarium", BotaniumMaster::new, machineItemCreator(), BotariumTile::new).register(BLOCKS, ITEMS, TILES);
+    public static final BlockRegistryObjectGroup<BotariumMaster, BlockItem, BotariumTile> BOTARIUM =
+            new BlockRegistryObjectGroup<>("botarium", BotariumMaster::new, machineItemCreator(), BotariumTile::new).register(BLOCKS, ITEMS, TILES);
 
     public static final RegistryObject<BotariumTop> BOTARIUM_TOP = BLOCKS.register("botarium_top", BotariumTop::new);
 
     public static final RegistryObject<TileEntityType<TopEnabledOnlySlave>> BOTARIUM_TOP_TILE = TILES.register("botarium_top", () -> TileEntityType.Builder.create(TopEnabledOnlySlave::new,BOTARIUM_TOP.get())
             .build(null));
 
-    public static <B extends Block> Function<B, BlockItem> blockItemCreator() {
+    public static <B extends MachineBlock> Function<B, BlockItem> blockItemCreator() {
         return block -> new BlockItem(block, new Item.Properties().group(TECHARIUMS));
     }
 
-    public static <B extends Block> Function<B, BlockItem> machineItemCreator() {
+    public static <B extends MachineBlock> Function<B, BlockItem> machineItemCreator() {
         return block -> new MachineItem(block, new Item.Properties().group(TECHARIUMS));
     }
 
