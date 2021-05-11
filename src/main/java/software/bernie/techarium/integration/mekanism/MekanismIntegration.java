7779bb232874c6178a15b00b5f7f550156b3f0f5
GeckoThePecko <bernie.gandin2@gmail.com>
2021-05-11 10:52:58 -0400

    Added integration API and lombok (#8)
    * Added integration API and lombok
    
    * Made integrations use Lazy's
    
    * Made modintegrations use LazyOptionals
    
    * Fixed typo

---
+++ src/main/java/software/bernie/techarium/integration/mekanism/MekanismIntegration.java
@@ -0,0 +1,14 @@
+package software.bernie.techarium.integration.mekanism;
+
+import mekanism.common.registries.MekanismItems;
+import net.minecraft.item.Item;
+import software.bernie.techarium.integration.Integration;
+
+/**
+ * Anything in this class is safe to use without worrying about ClassNotfound's, the Wrapper takes care of the overhead
+ */
+public class MekanismIntegration extends Integration {
+    public boolean isGauge(Item item) {
+        return item == MekanismItems.GAUGE_DROPPER.getItem();
+    }
+}
