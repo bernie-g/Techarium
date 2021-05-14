9f749ad030bb6238033e169350350bd8bda023fe
agnor99 <kreader100@gmail.com>
2021-05-14 21:23:16 +0200

    removed import

--- src/main/java/software/bernie/techarium/datagen/TechariumRecipeProvider.java
+++ src/main/java/software/bernie/techarium/datagen/TechariumRecipeProvider.java
@@ -1,9 +1,7 @@
 package software.bernie.techarium.datagen;
 
 import net.minecraft.block.Blocks;
-import net.minecraft.block.CropsBlock;
 import net.minecraft.block.FlowerBlock;
-import net.minecraft.block.StemBlock;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.fluid.Fluids;
@@ -13,10 +11,8 @@ import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.crafting.conditions.ModLoadedCondition;
 import net.minecraftforge.common.data.ForgeRecipeProvider;
 import net.minecraftforge.fluids.FluidStack;
-import org.checkerframework.checker.units.qual.C;
 import software.bernie.techarium.Techarium;
 import software.bernie.techarium.integration.ModIntegrations;
 import software.bernie.techarium.recipes.recipe.BotariumRecipe;
