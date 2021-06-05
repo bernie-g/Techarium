1cd4b2358535b1ebb4b61bcebc678cd55972c010
agnor99 <36055315+agnor99@users.noreply.github.com>
2021-06-05 20:26:05 +0200

    Fixed some small issues and added partial voxelshapes (#34)
    * Fixed some small issues and added part of the voxelshapes

--- src/main/java/software/bernie/techarium/util/Utils.java
+++ src/main/java/software/bernie/techarium/util/Utils.java
@@ -4,7 +4,11 @@ import com.google.gson.JsonObject;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.Direction;
 import net.minecraft.util.JSONUtils;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.text.IFormattableTextComponent;
 import net.minecraft.util.text.ITextComponent;
 
@@ -29,5 +33,29 @@ public class Utils {
     public static Ingredient deserializeIngredient(JsonObject json, String key) {
         return Ingredient.deserialize((JSONUtils.isJsonArray(json, key) ? JSONUtils.getJsonArray(json,
                 "soilIn") : JSONUtils.getJsonObject(json, key)));
+    }
+
+    /**
+     * Rotates a VoxelShape around the center to the specified Direction, Origin is SOUTH
+     * @param toRotate
+     * @param direction
+     * @return the rotated VoxelShape
+     */
+    public static VoxelShape rotateVoxelShape(VoxelShape toRotate, Direction direction) {
+        VoxelShape[] buffer = new VoxelShape[]{toRotate, VoxelShapes.empty() };
+        if (direction.getHorizontalIndex() == -1) {
+            if (direction == Direction.DOWN) {
+                buffer[0].forEachBox((minX, minY, minZ, maxX, maxY, maxZ) -> buffer[1] = VoxelShapes.or(buffer[1], VoxelShapes.create(minX, 1-maxZ, minY, maxX, 1 - minZ, maxY)));
+            } else {
+                buffer[0].forEachBox((minX, minY, minZ, maxX, maxY, maxZ) -> buffer[1] = VoxelShapes.or(buffer[1], VoxelShapes.create(minX, minZ, minY, maxX, maxZ, maxY)));
+            }
+            return buffer[1];
+        }
+        for (int i = 0; i < direction.getHorizontalIndex() % 4; i++) {
+            buffer[0].forEachBox((minX, minY, minZ, maxX, maxY, maxZ) -> buffer[1] = VoxelShapes.or(buffer[1], VoxelShapes.create(1-maxZ, minY, minX, 1-minZ, maxY, maxX)));
+            buffer[0] = buffer[1];
+            buffer[1] = VoxelShapes.empty();
+        }
+        return buffer[0];
     }
 }
